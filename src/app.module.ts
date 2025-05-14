929003d444d5283c26b627a65cc0a568f3f1578e
omitul.islam <omitul.islam@brainstation-23.com>
2025-05-14 16:02:23 +0600

    configured docker-compose.yml file and pushed image to dock.hub
    now data is persistant in docker, mapped with local db.sqlite inside data folder, used bind mounts, also pushed the image to the dockerHub

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { User } from './auth/entities/user.entity';
+  import { Module } from '@nestjs/common';
+  import { AppController } from './app.controller';
+  import { AppService } from './app.service';
+  import { TypeOrmModule } from '@nestjs/typeorm';
+  import { AuthModule } from './auth/auth.module';
+  import { ConfigModule } from '@nestjs/config';
+  import { User } from './auth/entities/user.entity';
 
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, 
+  @Module({
+    imports: [
+      ConfigModule.forRoot({
+        isGlobal: true, 
+      }),
+      TypeOrmModule.forRoot({
+        type: 'sqlite',
+        database: 'data/db.sqlite',
+        entities: [User],
+        synchronize: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User],
-      synchronize: true,
-  }),
-    AuthModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+      AuthModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+  })
+  export class AppModule {}
